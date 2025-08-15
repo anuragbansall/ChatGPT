f4c15f84ce9dca3297cced23928141490a099f2b
Anurag Bansal <devanuragbansal@gmail.com>
2025-08-16 01:40:51 +0530

    fix: correct formatting of title field in Conversation model

--- server/src/models/conversation.model.js
+++ server/src/models/conversation.model.js
@@ -3,12 +3,12 @@ import mongoose from "mongoose";
 const conversationSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    title: { 
-      type: String, 
-      required: true, 
-      trim: true, 
-      minlength: 1, 
-      maxlength: 100 
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 100,
     },
   },
   {
