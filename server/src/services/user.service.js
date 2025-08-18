cd42199c200a86b6e701dbe756e86bb7bb8b46de
Anurag Bansal <devanuragbansal@gmail.com>
2025-08-18 16:19:12 +0530

    feat: add user not found error handling in getUserById function

--- server/src/services/user.service.js
+++ server/src/services/user.service.js
@@ -13,6 +13,11 @@ export const createUser = async (userData) => {
 export const getUserById = async (userId) => {
   try {
     const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
     return user;
   } catch (error) {
     throw error;
